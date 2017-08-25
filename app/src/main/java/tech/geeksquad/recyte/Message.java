1e967f1f1f26f9b0da6552bf738ec74d74615e2a
sanjit <sanjitschouhan@gmail.com>
2017-08-26 04:35:22 +0530

    chat

--- app/src/main/java/tech/geeksquad/recyte/Message.java
+++ app/src/main/java/tech/geeksquad/recyte/Message.java
@@ -3,14 +3,31 @@ package tech.geeksquad.recyte;
 public class Message {
     private String author;
     private String message;
+    private boolean isImage;
+    private byte[] image;
 
     public Message(String author, String message) {
         this.author = author;
         this.message = message;
+        isImage = false;
+    }
+
+    public Message(String author, byte[] image) {
+        this.author = author;
+        this.image = image;
+        isImage = true;
     }
 
     public Message() {
 
+    }
+
+    public boolean isImage() {
+        return isImage;
+    }
+
+    public byte[] getImage() {
+        return image;
     }
 
     public String getAuthor() {
