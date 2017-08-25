cea523754f90d1fee519e790ab7f1cee98de103c
biswajeet619 <biswajeet15696@gmail.com>
2017-08-25 23:40:17 +0530

    new activity

--- app/src/main/java/tech/geeksquad/recyte/DashBoardActivity.java
+++ app/src/main/java/tech/geeksquad/recyte/DashBoardActivity.java
@@ -1,7 +1,9 @@
 package tech.geeksquad.recyte;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashBoardActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class DashBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+    }
+
+    public void profile(View view) {
+        startActivity(new Intent(this, ProfileActivity.class));
+    }
+
+    public void bot(View view) {
+        startActivity(new Intent(this,BotActivity.class));
+    }
+
+    public void orders(View view) {
+        startActivity(new Intent(this,OrdersActivity.class));
+    }
+
+    public void industry(View view) {
+        startActivity(new Intent(this,IndustryContactActivity.class));
     }
 }
