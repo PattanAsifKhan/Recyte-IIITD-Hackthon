578e55b5218b8936fb82228b6f731d80a040d2a0
sanjit <sanjitschouhan@gmail.com>
2017-08-26 09:17:25 +0530

    chat

---
+++ app/src/main/java/tech/geeksquad/recyte/PlacesNearMeActivity.java
@@ -0,0 +1,13 @@
+package tech.geeksquad.recyte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlacesNearMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_near_me);
+    }
+}
