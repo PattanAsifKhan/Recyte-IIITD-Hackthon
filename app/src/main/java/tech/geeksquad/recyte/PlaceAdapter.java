104689f9b7d7277dc275db88c4e2080c1f8d3908
sanjit <sanjitschouhan@gmail.com>
2017-08-26 10:19:29 +0530

    chat

--- app/src/main/java/tech/geeksquad/recyte/PlaceAdapter.java
+++ app/src/main/java/tech/geeksquad/recyte/PlaceAdapter.java
@@ -1,53 +1,28 @@
 package tech.geeksquad.recyte;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.List;
 
-public class PlaceAdapter extends ArrayAdapter<Message> {
+public class PlaceAdapter extends ArrayAdapter<Places> {
 
-    public PlaceAdapter(@NonNull Context context, @NonNull List<Message> objects) {
+    public PlaceAdapter(@NonNull Context context, @NonNull List<Places> objects) {
         super(context, 0, objects);
     }
 
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Message item = getItem(position);
+        Places item = getItem(position);
         View view = convertView;
         if (item != null) {
-            TextView textView;
-            ImageView imageView = null;
-            if (item.getAuthor().equalsIgnoreCase("user")) {
-                view = LayoutInflater.from(getContext()).inflate(R.layout.adapterright, null);
-                textView = (TextView) view.findViewById(R.id.usertext);
-                imageView = (ImageView) view.findViewById(R.id.userimage);
-            } else {
-                view = LayoutInflater.from(getContext()).inflate(R.layout.adapterleft, null);
-                textView = (TextView) view.findViewById(R.id.bottext);
-            }
-
-            if (item.getIsImage() == 1) {
-                byte[] image = Base64.decode(item.getData(), Base64.DEFAULT);
-                Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
-                imageView.setImageBitmap(bitmap);
-                imageView.setVisibility(View.VISIBLE);
-                textView.setVisibility(View.GONE);
-            } else {
-                textView.setText(item.getMessage());
-                if (imageView != null)
-                    imageView.setVisibility(View.GONE);
-                textView.setVisibility(View.VISIBLE);
+            if (view == null) {
+                LayoutInflater.from(getContext()).inflate();
             }
         }
         return view;
